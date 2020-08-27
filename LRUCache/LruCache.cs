edca453c7c65329b0cf322bb91e4cb682e335c24
Alex Shmukler <alexshmukler@Alexs-MacBook-Pro.local>
2020-08-27 18:53:57 +0300

    first commit

---
+++ LRUCache/LruCache.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+
+namespace LRUCache
+{
+    public class LruCache<TKey, TValue>
+    {
+        private readonly int Capacity;
+
+        private readonly Dictionary<TKey, LinkedListNode<LruCacheItem>> CacheMap = new Dictionary<TKey, LinkedListNode<LruCacheItem>>();
+
+        private readonly LinkedList<LruCacheItem> LruList = new LinkedList<LruCacheItem>();
+
+        private static readonly object LockObject = new object();
+
+        public LruCache(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            lock (LockObject)
+            {
+                if (CacheMap.TryGetValue(key, out LinkedListNode<LruCacheItem> node))
+                {
+                    value = node.Value.Value;
+
+                    LruList.Remove(node);
+
+                    LruList.AddLast(node);
+
+                    return true;
+                }
+
+                value = default;
+
+                return false;
+            }
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            lock (LockObject)
+            {
+                if (CacheMap.Count >= Capacity)
+                {
+                    RemoveFirst();
+                }
+
+                LinkedListNode<LruCacheItem> node = new LinkedListNode<LruCacheItem>(new LruCacheItem(key, value));
+
+                LruList.AddLast(node);
+
+                CacheMap.Add(key, node);
+            }
+        }
+
+        private void RemoveFirst()
+        {
+            CacheMap.Remove(LruList.First.Value.Key);
+
+            LruList.RemoveFirst();
+        }
+
+        private class LruCacheItem
+        {
+            public TKey Key { get; }
+
+            public TValue Value { get; }
+
+            public LruCacheItem(TKey key, TValue value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ LRUCache/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LRUCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LruCache<string, object> lruCache = new LruCache<string, object>(2);
+
+            lruCache.Add("item1", new object());
+
+            lruCache.Add("item2", new object());
+
+            lruCache.Add("item3", new object());
+
+            if (lruCache.TryGetValue("item1", out object item1))
+            {
+                // item1 not exist
+            }
+            else
+            {
+                lruCache.Add("item1", new object());
+            }
+        }
+    }
+}
